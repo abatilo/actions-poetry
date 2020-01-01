f2af45b720c8a555d9a220dad84a5b5ee705c938
Nick Pleatsikas <nick@pleatsikas.me>
2020-01-01 14:40:34 -0800

    feat: Adds working_directory Input to Action (#14)
    This allows the user to set the directory in which poetry commands are
    run in. This enabled compatibility for projects with no pyproject.toml
    in root of the repository.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-pythonVersion="$INPUT_PYTHON_VERSION"
-poetryVersion="$INPUT_POETRY_VERSION"
-pyenv latest install $pythonVersion
-pyenv latest global $pythonVersion
+pyenv latest install "$INPUT_PYTHON_VERSION"
+pyenv latest global "$INPUT_PYTHON_VERSION"
 pip install -r /requirements.txt
-pip install poetry==$poetryVersion
+pip install poetry=="$INPUT_POETRY_VERSION"
 pyenv rehash
 
+# Push current directory on to stack and cd (if possible) into working dir.
+pushd . > /dev/null 2>&1 || return
+cd "$INPUT_WORKING_DIRECTORY" || return
+
+# Fix for virtualenvs defined in .python-version.
+pyenv local "$INPUT_PYTHON_VERSION"
+
 sh -c "poetry $*"
+
+# Step back to starting directory.
+popd > /dev/null 2>&1 || return
