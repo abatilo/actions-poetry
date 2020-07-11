45e193a0e0057bc99a465d20f9ec51fc2f2ed35f
xushanchuan <aisensiy@163.com>
2020-07-11 17:26:37 +0800

    fix: remove pycache to aviod further permission error

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,5 +15,8 @@ pyenv latest local "$INPUT_PYTHON_VERSION"
 
 sh -c "poetry $*"
 
+# Clean up __pycache__
+find . -type d -name  "__pycache__" -exec rm -r {} +
+
 # Step back to starting directory.
 popd > /dev/null 2>&1 || return
